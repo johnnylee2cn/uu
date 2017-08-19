061c48cc779a41444a5d4f80ae7afe57c2ca95f3
johnnylee2cn <839434946@qq.com>
2017-08-19 19:48:30 +0800

    网络状态监听

--- UU/LoginViewController.swift
+++ UU/LoginViewController.swift
@@ -72,6 +72,7 @@ class LoginViewController: UIViewController,UIViewControllerTransitioningDelegat
         return transition
     }
     
+    
     func networkStatusListener(){
         //1.设置网络监听状态 2。获得网络Reachability对象
         NotificationCenter.default.addObserver(self, selector: #selector(self.reachabilityChanged),name: ReachabilityChangedNotification,object: reachability)
@@ -92,29 +93,60 @@ class LoginViewController: UIViewController,UIViewControllerTransitioningDelegat
             print("网络连接：可用")
             if reachability.isReachableViaWiFi { // 判断网络连接类型
                 print("连接类型：WiFi")
+                DispatchQueue.main.async {
+                    self.alert_noNetwrok(statu: "正在使用wifi")
+                }
                 // strServerInternetAddrss = getHostAddress_WLAN() // 获取主机IP地址 192.168.31.2 小米路由器
                 // processClientSocket(strServerInternetAddrss)    // 初始化Socket并连接，还得恢复按钮可用
             } else {
                 print("连接类型：移动网络")
+                DispatchQueue.main.async {
+                    self.alert_noNetwrok(statu: "正在使用数据流量")
+                }
                 // getHostAddrss_GPRS()  // 通过外网获取主机IP地址，并且初始化Socket并建立连接
             }
         } else {
             print("网络连接：不可用")
             DispatchQueue.main.async { // 不加这句导致界面还没初始化完成就打开警告框，这样不行
-                self.alert_noNetwrok() // 警告框，提示没有网络
+                self.alert_noNetwrok(statu: "网络连接已关闭") // 警告框，提示没有网络
             }
         }
     }
     
     // 警告框，提示没有连接网络 *********************
-    func alert_noNetwrok() -> Void {
-        let alert = UIAlertController(title: "系统提示", message: "请打开网络连接", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "确定", style: .default, handler: nil)
-        alert.addAction(cancelAction)
-        self.present(alert, animated: true, completion: nil)
+    func alert_noNetwrok(statu:String) -> Void {
+        //获取当前用户浏览页面的控制器
+        let window = UIApplication.shared.keyWindow
+        let rootVC = window?.rootViewController
+        var vc:UIViewController!
+        
+        guard rootVC != nil else {
+            return
+        }
+        
+        if rootVC?.presentedViewController != nil{
+            vc = rootVC
+        }
+        
+        if rootVC?.isKind(of: UINavigationController.self) == true{
+            let nave = rootVC as! UINavigationController
+            vc = nave.visibleViewController
+        }
+        
+        if rootVC?.isKind(of: UITabBarController.self) == true{
+            let tab = (rootVC as! UITabBarController).selectedViewController
+            vc = tab
+        }
+        
+        //获取到以后，加上提示框
+        FTIndicator.setIndicatorStyle(.dark)
+        FTIndicator.showNotification(withTitle: "", message: statu)
+        
+        //        let alert = UIAlertController(title: "系统提示", message: statu, preferredStyle: .alert)
+        //        let cancelAction = UIAlertAction(title: "确定", style: .default, handler: nil)
+        //        alert.addAction(cancelAction)
+        //        vc.present(alert, animated: true, completion: nil)
     }
-    
-
     
     // MARK: - 画横线
     
